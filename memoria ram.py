095037bb1fa232a890f86f2a779c3fd8c648a9d3
raphapinho <73498424+raphapinho@users.noreply.github.com>
2021-01-19 13:44:14 -0300

    Add files via upload

---
+++ memoria ram.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+matriz = [[0,0,0,],[0,0,0],[0,0,0]]
+for l in range(0,3):
+    for c in range(0,3):
+        matriz[l][c]= int(input(f'digite o valor para [{l},{c}]:'))
+print('-=' * 30)
+for l in range(0,3):
+    for c in range(0,3):
+        print(f'[{matriz[l][c]}]',end='')
+    print()
+
+
+# In[ ]:
+
+
+
+
